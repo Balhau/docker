f8f082ce78f46a6fa44ed5eba3eb118dc2335c99
balhau <balhau@balhau.net>
2015-10-03 18:53:00 +0100

    Fix bugs on docker files

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,2 +1,2 @@
-/opt/kafka_2.10-0.8.2.1/bin/zookeeper-server-start.sh /opt/kafka_2.10-0.8.2.1/config/zookeeper.properties &
+/opt/kafka_2.10-0.8.2.1/bin/zookeeper-server-start.sh /opt/kafka_2.10-0.8.2.1/config/zookeeper.properties
 /opt/kafka_2.10-0.8.2.1/bin/kafka-server-start.sh /opt/kafka_2.10-0.8.2.1/config/server.properties &
