6ada697f0d262414aeaad960dd5101d7bed29b0b
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-10-17 16:19:18 +0100

    Changed start-server.sh in webdata to include the fetch of the last version of the binary

--- webdata/start-server.sh
+++ webdata/start-server.sh
@@ -1,1 +1,2 @@
+cd /var/app/webdata && wget "http://nexus.balhau.net/nexus/service/local/artifact/maven/redirect?r=snapshots&g=org.pt.pub.data&a=webpt-ws&v=LATEST" -O web-pt.jar
 java -jar /var/app/webdata/web-pt.jar
