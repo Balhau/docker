1640d92271243113d4977195964a5bcef6321a4c
Balhau <balhau@balhau.net>
2016-01-30 09:35:02 +0000

    Refactor docker setup for omxclient

--- omxclient/start-server.sh
+++ omxclient/start-server.sh
@@ -1,6 +1,7 @@
-pip uninstall -y omxclient
-pip install omxclient --upgrade --extra-index-url  http://pip.balhau.net/simple --trusted-host pip.balhau.net #
-python /usr/lib/python2.7/site-packages/omxclient/main.py
+#pip uninstall -y omxclient
+#pip install omxclient --upgrade --extra-index-url  http://pip.balhau.net/simple --trusted-host pip.balhau.net #
+#python /usr/lib/python2.7/site-packages/omxclient/main.py
+python /var/app/omxclient/omxclient/main.py
 tail -f /var/log/*log
 
 
